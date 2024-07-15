225891a42540a59089e76748a5d3cca4257cc58a
NataliiaKostiuk <128825690+NataliiaKostiuk@users.noreply.github.com>
2024-07-15 12:30:43 +0100

    delete concole

--- src/pages/CampersPage/CampersPage.jsx
+++ src/pages/CampersPage/CampersPage.jsx
@@ -23,12 +23,9 @@ const CampersPage = () => {
     };
     const filteredByLocation = campers && campers.filter(camper =>
         camper.location.toLowerCase().includes(city.toLowerCase()));
-    console.log(campers);
-    
 
     const handleSubmit = (values) => {
         const arrayKeys = Object.keys(values);
-        console.log(arrayKeys); 
         
         const filteredCampers = campers.filter(camper => {
             return arrayKeys.every(key => {
@@ -46,42 +43,8 @@ const CampersPage = () => {
 
         setfilterCampers(filteredCampers);
     
-    //     if (arrayKeys.includes('conditioner')) {
-    //         const filteredByConditioner = campers.filter(({ conditioner }) => conditioner !== 0)
-    //         setfilterCampers(filteredByConditioner);
-    //     }
-    //     if (arrayKeys.includes('automat')) {
-    //         const filteredByAutomat = filterCampers.filter(({ transmission }) => transmission === 'automat')
-    //         setfilterCampers(filteredByAutomat);
-    //         }
-    //     if (arrayKeys.includes('kitchen')) {
-    //         const filteredByKitchen = filterCampers.filter(({ kitchen }) => kitchen !== 0)
-    //         setfilterCampers(filteredByKitchen);
-    //     }
-    //     if (arrayKeys.includes('shower')) {
-    //         const filteredByShower = filterCampers.filter(({ shower }) => shower !== 0)
-    //         setfilterCampers(filteredByShower);
-    //     }
-    //     if (arrayKeys.includes('TW')) {
-    //         const filteredByTW = filterCampers.filter(({ TW}) => TW !== 0)
-    //         setfilterCampers(filteredByTW);
-    //     }
-    //     if (arrayKeys.includes('van')) {
-    //         const filteredByVan = filterCampers.filter(({ form }) => form === 'panelTruck')
-    //         setfilterCampers(filteredByVan);
-    //     } 
-    //      if (arrayKeys.includes('fullyIntegreted')) {
-    //         const filteredByFullyInt = filterCampers.filter(({ form }) => form === 'fullyIntegrated')
-    //         setfilterCampers(filteredByFullyInt);
-    //     } 
-    //      if (arrayKeys.includes('alcove')) {
-    //         const filteredByalcove = filterCampers.filter(({ form }) =>form === 'alcove')
-    //         setfilterCampers(filteredByalcove);
-    //         } 
-    //      console.log(filterCampers);
     }
 
- console.log(filterCampers);
 
     return (
         <Container>
